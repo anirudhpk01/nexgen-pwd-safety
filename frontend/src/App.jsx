c1d1bff5bcbd90ea1c228584d8b5c967bc582e6d
akshay-here <akshay.as2003@gmail.com>
2025-02-15 14:34:53 +0530

    pocketbase react-router and axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router"
+
 function App() {
-  return <h1 className="text-red-900" >Hello World</h1>
+  // return <h1 className="text-red-900" >Hello World</h1>
+  return (
+  <BrowserRouter>
+    <h1>Hello World</h1>
+  </BrowserRouter>
+  )
 }
 
 export default App
